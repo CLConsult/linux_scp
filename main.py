018eeb91eb787b87a4e822e51def371a43be2bd8
Gustavo Leig <gus.leig@gmail.com>
2022-08-24 12:32:13 -0300

    nova função de realizar backup para pasta YYYY-MM-DD

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from paramiko import SSHClient
 from scp import SCPClient
 import paramiko
 import api_keys
+import shutil
+import datetime
+import os
 
 host = api_keys.host
 port = api_keys.port
@@ -10,6 +13,29 @@ passwd = api_keys.passwd
 key_file = ""
 file_path = api_keys.origin_path
 dest_path = api_keys.dest_path
+
+
+def copytree(src, dst, extension='', copydir=False, symlinks=False, ignore=None):
+
+    for item in os.listdir(src):
+
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+
+        if os.path.isdir(s) and copydir:
+            shutil.copytree(s, d, symlinks, ignore)
+
+        elif not item.startswith("."):
+
+            if extension and item.endswith(extension):
+                if not os.path.exists(dst):
+                    os.makedirs(dst)
+                shutil.copy2(s, d)
+
+            elif not extension:
+                if not os.path.exists(dst):
+                    os.makedirs(dst)
+                shutil.copy2(s, d)
 
 
 if __name__ == '__main__':
@@ -24,3 +50,11 @@ if __name__ == '__main__':
             scp.get(remote_path=file_path, local_path=dest_path)
         except Exception as e:
             print(e)
+
+    dest_dir = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    print(f"Realiando copia para pasta diária: {dest_dir}")
+
+    copytree(dest_path, dest_dir)
+
+    print("fim")
