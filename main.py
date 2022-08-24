755b97933d7f30b76518241fd3b208b465983669
Gustavo Leig <gus.leig@gmail.com>
2022-08-24 13:15:24 -0300

    try catch para ssh.connect

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import api_keys
 import shutil
 import datetime
 import os
+import sys
 
 host = api_keys.host
 port = api_keys.port
@@ -45,7 +46,11 @@ if __name__ == '__main__':
 
     print(f"Conectando ao host: {host} Porta: {port} Usuario: {user} Senha: {passwd}")
 
-    ssh.connect(host, port=port, username=user, password=passwd)
+    try:
+        ssh.connect(host, port=port, username=user, password=passwd)
+    except Exception as e:
+        print(f"Erro: {e}")
+        sys.exit(-1)
 
     with SCPClient(ssh.get_transport(), sanitize=lambda x: x) as scp:
 
